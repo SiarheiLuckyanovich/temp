1d1fc17524d32ed45e6f89c4209fbb6778e21ab6
Siarhei Lukyanovich <codename_arhey@mail.ru>
2022-08-24 13:56:06 +0300

    new

---
+++ foo.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+    void checkarr(const float * arr, size_t size)
+{
+    int n = 0;
+    int p = 0;
+    for (size_t i = 0; i < size; i++)
+    {
+        if (arr[i] < 0)
+        {
+            n = n + 1;
+        }
+        else
+            p = p + 1;
+    }
+    cout << "Positive elements: " << p << ".  Negative elements: " << n << endl;
+}
+    void initarr (float * arr, size_t size)
+{
+    float srand();
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 100 - 50;
+    }
+
+}
+
+    void printarr(const float * arr, size_t size)
+{
+  for (size_t i = 0; i < size; i++)
+    {
+        cout << arr[i] << " " ;
+    }
+  cout << endl;
+}
+
+
---
+++ foo.h
@@ -0,0 +1,9 @@
+#ifndef FOO_H_INCLUDED
+#define FOO_H_INCLUDED
+#endif // FOO_H_INCLUDED
+#pragma once // include guard — защита от повторного включения заголовочного файла
+//void printArray(const int array[], int size); // Прототип функции
+
+void printarr(const float * arr, size_t size);
+void checkarr(const float * arr, size_t size);
+void initarr (float * arr, size_t size);
